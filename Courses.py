c8240768be0c0f9c73ab4c1e37c65c07441a2d39
Mohammed Fouzan <125375399+mohammedFouzan@users.noreply.github.com>
2025-04-30 15:35:48 +0530

    Add files via upload

---
+++ Courses.py
@@ -0,0 +1,152 @@
+ds_course = [['Machine Learning Crash Course by Google [Free]', 'https://developers.google.com/machine-learning/crash-course'],
+             ['Machine Learning A-Z by Udemy','https://www.udemy.com/course/machinelearning/'],
+             ['Machine Learning by Andrew NG','https://www.coursera.org/learn/machine-learning'],
+             ['Data Scientist Master Program of Simplilearn (IBM)','https://www.simplilearn.com/big-data-and-analytics/senior-data-scientist-masters-program-training'],
+             ['Data Science Foundations: Fundamentals by LinkedIn','https://www.linkedin.com/learning/data-science-foundations-fundamentals-5'],
+             ['Data Scientist with Python','https://www.datacamp.com/tracks/data-scientist-with-python'],
+             ['Programming for Data Science with Python','https://www.udacity.com/course/programming-for-data-science-nanodegree--nd104'],
+             ['Programming for Data Science with R','https://www.udacity.com/course/programming-for-data-science-nanodegree-with-R--nd118'],
+             ['Introduction to Data Science','https://www.udacity.com/course/introduction-to-data-science--cd0017'],
+             ['Intro to Machine Learning with TensorFlow','https://www.udacity.com/course/intro-to-machine-learning-with-tensorflow-nanodegree--nd230'], 
+            
+    ['Deep Learning Specialization by Andrew Ng', 'https://www.coursera.org/specializations/deep-learning'],
+    ['Applied Data Science with Python', 'https://www.coursera.org/specializations/data-science-python'],
+    ['Python for Data Science and Machine Learning Bootcamp', 'https://www.udemy.com/course/python-for-data-science-and-machine-learning-bootcamp/'],
+    ['Google Data Analytics Professional Certificate', 'https://www.coursera.org/professional-certificates/google-data-analytics'],
+    ['Harvard’s Data Science Professional Certificate', 'https://pll.harvard.edu/series/data-science-professional-certificate']]
+
+
+
+
+
+web_course = [['Django Crash course [Free]','https://youtu.be/e1IyzVyrLSU'],
+              ['Python and Django Full Stack Web Developer Bootcamp','https://www.udemy.com/course/python-and-django-full-stack-web-developer-bootcamp'],
+              ['React Crash Course [Free]','https://youtu.be/Dorf8i6lCuk'],
+              ['ReactJS Project Development Training','https://www.dotnettricks.com/training/masters-program/reactjs-certification-training'],
+              ['Full Stack Web Developer - MEAN Stack','https://www.simplilearn.com/full-stack-web-developer-mean-stack-certification-training'],
+              ['Node.js and Express.js [Free]','https://youtu.be/Oe421EPjeBE'],
+              ['Flask: Develop Web Applications in Python','https://www.educative.io/courses/flask-develop-web-applications-in-python'],
+              ['Full Stack Web Developer by Udacity','https://www.udacity.com/course/full-stack-web-developer-nanodegree--nd0044'],
+              ['Front End Web Developer by Udacity','https://www.udacity.com/course/front-end-web-developer-nanodegree--nd0011'],
+              ['Become a React Developer by Udacity','https://www.udacity.com/course/react-nanodegree--nd019'],
+               ['Full Stack Open by University of Helsinki [Free]', 'https://fullstackopen.com/en/'],
+    ['The Complete JavaScript Course by Udemy', 'https://www.udemy.com/course/the-complete-javascript-course/'],
+    ['Modern React with Redux', 'https://www.udemy.com/course/react-redux/'],
+    ['JavaScript Algorithms and Data Structures by freeCodeCamp [Free]', 'https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/'],
+    ['CSS - The Complete Guide (incl. Flexbox, Grid & Sass)', 'https://www.udemy.com/course/css-the-complete-guide-incl-flexbox-grid-sass/']]
+
+
+
+
+android_course = [['Android Development for Beginners [Free]','https://youtu.be/fis26HvvDII'],
+                  ['Android App Development Specialization','https://www.coursera.org/specializations/android-app-development'],
+                  ['Associate Android Developer Certification','https://grow.google/androiddev/#?modal_active=none'],
+                  ['Become an Android Kotlin Developer by Udacity','https://www.udacity.com/course/android-kotlin-developer-nanodegree--nd940'],
+                  ['Android Basics by Google','https://www.udacity.com/course/android-basics-nanodegree-by-google--nd803'],
+                  ['The Complete Android Developer Course','https://www.udemy.com/course/complete-android-n-developer-course/'],
+                  ['Building an Android App with Architecture Components','https://www.linkedin.com/learning/building-an-android-app-with-architecture-components'],
+                  ['Android App Development Masterclass using Kotlin','https://www.udemy.com/course/android-oreo-kotlin-app-masterclass/'],
+                  ['Flutter & Dart - The Complete Flutter App Development Course','https://www.udemy.com/course/flutter-dart-the-complete-flutter-app-development-course/'],
+                  ['Flutter App Development Course [Free]','https://youtu.be/rZLR5olMR64'], 
+                  
+                  ['Android App Development for Beginners by freeCodeCamp [Free]', 'https://youtu.be/mXjZQX3UzOs'],
+    ['Kotlin for Android Developers', 'https://developer.android.com/kotlin'],
+    ['The Complete Flutter Development Bootcamp', 'https://www.udemy.com/course/flutter-bootcamp-with-dart/'],
+    ['Android Jetpack Compose - Modern Android UI Development', 'https://www.udemy.com/course/android-jetpack-compose-crash-course/'],
+    ['Google’s Android Developer Fundamentals', 'https://developer.android.com/courses/fundamentals-training/toc']]
+
+
+
+
+software_dev_course = [
+    # General Software Development
+    ['Software Development Lifecycle [Free]', 'https://www.udacity.com/course/software-development-process--ud805'],
+    ['Software Engineering for Beginners [Free]', 'https://youtu.be/DIoQ2ZpT9Q8'],
+    ['The Complete Software Developer Bootcamp', 'https://www.udemy.com/course/software-developer-bootcamp/'],
+    ['Become a Software Engineer by Udacity', 'https://www.udacity.com/course/software-engineer-nanodegree--nd035'],
+    ['Agile Software Development', 'https://www.coursera.org/specializations/agile-development'],
+
+    # Web Development
+    ['The Complete Web Developer Course', 'https://www.udemy.com/course/the-complete-web-developer-course-2/'],
+    ['Full Stack Web Developer by Udacity', 'https://www.udacity.com/course/full-stack-web-developer-nanodegree--nd0044'],
+    ['Web Development Bootcamp', 'https://www.udemy.com/course/the-web-developer-bootcamp/'],
+    ['Modern React with Redux', 'https://www.udemy.com/course/react-redux/'],
+    ['Node.js: The Complete Guide', 'https://www.udemy.com/course/nodejs-the-complete-guide/'],
+
+    # Backend Development
+    ['Node.js and Express - Full Course [Free]', 'https://youtu.be/Oe421EPjeBE'],
+    ['Django for Beginners', 'https://www.udemy.com/course/python-and-django-full-stack-web-developer-bootcamp/'],
+    ['Spring Boot & Microservices', 'https://www.udemy.com/course/microservices-with-spring-boot-and-spring-cloud/'],
+    ['REST API Development with Flask and Python', 'https://www.udemy.com/course/rest-api-flask-and-python/'],
+    ['Mastering GraphQL with Apollo', 'https://www.udemy.com/course/graphql-by-example/'],
+
+    # DevOps & Cloud
+    ['DevOps Beginner’s Guide [Free]', 'https://www.udacity.com/course/devops-nanodegree--nd9991'],
+    ['AWS Certified Developer', 'https://www.udemy.com/course/aws-certified-developer-associate/'],
+    ['Docker & Kubernetes Complete Guide', 'https://www.udemy.com/course/docker-and-kubernetes-the-complete-guide/'],
+    ['CI/CD Pipelines with Jenkins', 'https://www.udemy.com/course/learn-devops-ci-cd-with-jenkins-using-pipelines-and-docker/'],
+    ['Google Cloud Professional Developer', 'https://www.coursera.org/professional-certificates/cloud-engineering-gcp'],
+
+    # Software Testing & QA
+    ['Automated Software Testing with Selenium', 'https://www.udemy.com/course/selenium-webdriver-tutorial/'],
+    ['Test-Driven Development (TDD) with Python', 'https://www.udemy.com/course/test-driven-development-with-python/'],
+    ['Cypress: End-to-End Testing for Web Apps', 'https://www.udemy.com/course/cypress-web-automation-testing/'],
+    ['API Testing with Postman', 'https://www.udemy.com/course/rest-api-testing-using-postman/'],
+    ['Agile Testing and Test Automation', 'https://www.coursera.org/learn/agile-testing']
+]
+
+
+
+
+
+ios_course = [['IOS App Development by LinkedIn','https://www.linkedin.com/learning/subscription/topics/ios'],
+              ['iOS & Swift - The Complete iOS App Development Bootcamp','https://www.udemy.com/course/ios-13-app-development-bootcamp/'],
+              ['Become an iOS Developer','https://www.udacity.com/course/ios-developer-nanodegree--nd003'],
+              ['iOS App Development with Swift Specialization','https://www.coursera.org/specializations/app-development'],
+              ['Mobile App Development with Swift','https://www.edx.org/professional-certificate/curtinx-mobile-app-development-with-swift'],
+              ['Swift Course by LinkedIn','https://www.linkedin.com/learning/subscription/topics/swift-2'],
+              ['Objective-C Crash Course for Swift Developers','https://www.udemy.com/course/objectivec/'],
+              ['Learn Swift by Codecademy','https://www.codecademy.com/learn/learn-swift'],
+              ['Swift Tutorial - Full Course for Beginners [Free]','https://youtu.be/comQ1-x2a1Q'],
+              ['Learn Swift Fast - [Free]','https://youtu.be/FcsY1YPBwzQ'], 
+
+              ['iOS Development for Beginners [Free]', 'https://developer.apple.com/learn/curriculum/'],
+    ['SwiftUI for Beginners', 'https://www.udemy.com/course/swiftui-for-beginners/'],
+    ['Build iOS Apps with SwiftUI and Combine', 'https://www.udemy.com/course/swiftui-ios13/'],
+    ['Stanford iOS Development Course [Free]', 'https://cs193p.sites.stanford.edu/'],
+    ['iOS & SwiftUI: The Complete iOS App Development Guide', 'https://www.udemy.com/course/ios13-app-development-bootcamp/']]
+
+
+
+
+
+uiux_course = [['Google UX Design Professional Certificate','https://www.coursera.org/professional-certificates/google-ux-design'],
+               ['UI / UX Design Specialization','https://www.coursera.org/specializations/ui-ux-design'],
+               ['The Complete App Design Course - UX, UI and Design Thinking','https://www.udemy.com/course/the-complete-app-design-course-ux-and-ui-design/'],
+               ['UX & Web Design Master Course: Strategy, Design, Development','https://www.udemy.com/course/ux-web-design-master-course-strategy-design-development/'],
+               ['The Complete App Design Course - UX, UI and Design Thinking','https://www.udemy.com/course/the-complete-app-design-course-ux-and-ui-design/'],
+               ['DESIGN RULES: Principles + Practices for Great UI Design','https://www.udemy.com/course/design-rules/'],
+               ['Become a UX Designer by Udacity','https://www.udacity.com/course/ux-designer-nanodegree--nd578'],
+               ['Adobe XD Tutorial: User Experience Design Course [Free]','https://youtu.be/68w2VwalD5w'],
+               ['Adobe XD for Beginners [Free]','https://youtu.be/WEljsc2jorI'],
+               ['Adobe XD in Simple Way','https://learnux.io/course/adobe-xd'],
+               ['Interaction Design Specialization', 'https://www.coursera.org/specializations/interaction-design'],
+    ['User Experience (UX) Certificate by Google', 'https://grow.google/certificates/ux-design/'],
+    ['Figma UI UX Design Essentials', 'https://www.udemy.com/course/figma-ux-ui-design-essential-training/'],
+    ['Sketch from A to Z: UI/UX Design', 'https://www.udemy.com/course/sketch-from-a-to-z/'],
+    ['Web Design for Beginners: Real World Coding in HTML & CSS', 'https://www.udemy.com/course/web-design-for-beginners-real-world-coding-in-html-css/']]
+
+
+
+
+resume_videos = ['https://youtu.be/y8YH0Qbu5h4','https://youtu.be/J-4Fv8nq1iA',
+                 'https://youtu.be/yp693O87GmM','https://youtu.be/UeMmCex9uTU',
+                 'https://youtu.be/dQ7Q8ZdnuN0','https://youtu.be/HQqqQx5BCFY',
+                 'https://youtu.be/CLUsplI4xMU','https://youtu.be/pbczsLkv7Cc']
+
+
+
+interview_videos = ['https://youtu.be/Ji46s5BHdr0','https://youtu.be/seVxXHi2YMs',
+                    'https://youtu.be/9FgfsLa_SmY','https://youtu.be/2HQmjLu-6RQ',
+                    'https://youtu.be/DQd_AlIvHUw','https://youtu.be/oVVdezJ0e7w'
+                    'https://youtu.be/JZK1MZwUyUU','https://youtu.be/CyXLhHQS3KY']
---
+++ README.md
@@ -0,0 +1,40 @@
+# Smart Resume Analyzer Application
+
+[![forthebadge made-with-python](http://ForTheBadge.com/images/badges/made-with-python.svg)](https://www.python.org/)                 
+[![Python 3.6](https://img.shields.io/badge/python-3.6-blue.svg)](https://www.python.org/downloads/release/python-360/)   
+
+
+## Source
+- Extracting user's information from the Resume, I used [PyResparser](https://omkarpathak.in/pyresparser/)
+- Extracting Resume PDF into Text, I used [PDFMiner](https://pypi.org/project/pdfminer/).
+
+## Features
+- User & Admin Section
+- Resume Score
+- Career Recommendations
+- Resume writing Tips suggestions
+- Courses Recommendations
+- Skills Recommendations
+- Youtube video recommendations
+
+## Usage
+- Open CMD in working directory.
+
+- Run following command
+  ```
+  pip install -r requirements.txt
+  ```
+- Downloads necessary libraries
+
+- `App.py` is the main Python file of Streamlit Web-Application. 
+
+- `Courses.py` is the Python file that contains courses and youtube video links.
+
+- Download XAMPP or any other control panel, and turn on the Apache & SQL service.
+
+- To run app, write following command in CMD. or use any IDE.
+  ```
+  streamlit run App.py
+  ```
+
+- `Uploaded_Resumes` folder contains the user uploaded resumes.
